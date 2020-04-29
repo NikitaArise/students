c355c5b8cb236ca7581e56d2778b0ff04dd170c5
Nikita Arkhipov <mister.nikitochka@gmail.com>
2020-04-29 18:07:24 +0700

    initial project

---
+++ Student.php
@@ -0,0 +1,11 @@
+<?php
+class Student{
+    private $name;
+    private $surname;
+    private $gender;
+    private $group;
+    private $email;
+    private $useResult;
+    private $yearBirth;
+    
+}
